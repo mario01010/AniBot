61c4d7e5ca3a407eba16dff1dd438b38c811802e
mario01010 <98655372+mario01010@users.noreply.github.com>
2024-06-27 22:50:15 +0200

    Update moderation-event.py

--- moderation-event.py
+++ moderation-event.py
@@ -5,25 +5,7 @@ class ModEvent(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.paroleNO = [
-            "cazzo", "c4zzo", "merda", "m3rda", "stronzo", "str0nz0", "bastardo", "b4st4rd0",
-            "puttana", "putt4n4", "troia", "tr0i4", "vaffanculo", "v4ff4nculo", "coglione", "c0gli0ne",
-            "testa di cazzo", "t3st4 di c4zzo", "figlio di puttana", "figli0 di putt4n4", "frocio", "fr0ci0",
-            "lesbica", "l3sbic4", "merdoso", "m3rd0s0", "culattone", "cul4tt0ne", "cazzone", "c4zz0ne",
-            "puttanella", "putt4n3ll4", "zoccola", "z0cc0l4", "fanculo", "f4nculo", "deficiente", "d3ficient3",
-            "cretino", "cr3tin0", "idiota", "idi0t4", "palle", "p4lle", "minchia", "minchi4", "pezzo di merda", 
-            "p3zz0 di m3rda", "cazzata", "c4zz4ta", "rompipalle", "r0mpip4lle", "rompicoglioni", "r0mpic0gli0ni",
-            "bastarda", "b4st4rd4", "scemo", "sc3m0", "stupido", "stup1d0", "imbecille", "imb3cill3", "feccia", 
-            "f3cci4", "demente", "d3m3nt3", "tonta", "t0nt4", "cretina", "cr3tin4", "zoccolo", "z0cc0l0",
-            "culona", "cul0n4", "stronza", "str0nz4", "sfigato", "sf1g4to", "suca", "suc4", "testa di minchia",
-            "t3st4 di minchi4", "pirla", "p1rl4", "scopare", "sc0p4re", "bocchinaro", "b0cchin4r0", "bocchinara",
-            "b0cchin4r4", "piscia", "pisci4", "merdata", "m3rd4t4", "cacca", "c4cc4", "cacarella", "c4c4rell4",
-            "sputo", "spu70", "merdina", "m3rd1n4", "cagare", "c4g4re", "scoreggia", "sc0r3ggi4", "pervertito",
-            "p3rv3rt1t0", "puttanata", "putt4n4t4", "sputtanare", "spu774n4r3", "impalare", "1mp4l4r3", "masturbarsi",
-            "m4sturbar51", "onanismo", "0n4n1sm0", "pederasta", "p3d3r4st4", "pipparolo", "p1pp4r0l0", "porco",
-            "p0rc0", "sborrata", "sb0rr4t4", "scopata", "sc0p4t4", "scroto", "scr0t0", "succhiacazzi", "succhi4c4zz1",
-            "succhiaminchia", "succhi4minchi4", "pompino", "p0mpin0", "sega", "s3g4", "sodomita", "s0d0mit4",
-            "stronzata", "str0nz4t4", "troiata", "tr0i4t4", "urina", "ur1n4", "zuzzurro", "zuzzurr0", "zuzzurellone",
-            "zuzzur3ll0n3"
+            "Banned words"
         ]
         
     @commands.Cog.listener()
